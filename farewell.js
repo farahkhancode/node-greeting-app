7b481cd6ec23652b8778bc943d926d27b69aad17
farahmed@gmail.com <farahmed@gmail.com>
2018-06-06 09:24:42 -0500

    first commit

---
+++ farewell.js
@@ -0,0 +1,7 @@
+exports.spanishFarewell = function() {
+  console.log("Adios");
+}
+
+exports.japaneseFarewell = function() {
+  console.log("Sayonara");
+}
---
+++ main.js
@@ -0,0 +1,17 @@
+const greetings = require("./greeting.js");
+const farewells = require("./farewell.js");
+
+// "Hello"
+greetings.greetingEnglish();
+
+// "Konichiwa"
+greetings.greetingJapanese();
+
+
+//"Hola"
+greetings.greetingSpanish();
+
+//"Hallo"
+greetings.greetingIcelandic();
+
+farewells.japaneseFarewell();
